126e18b144237eb0322d738e6e6766df3f6a3e5c
Brian Adams <bpeteradams@gmail.com>
2022-04-14 20:46:23 -0400

    Scripts(watch): Added watch script to the turbo pipeline and added watch script to dnd-5e-api package

--- apps/dnd-5e-api/src/index.ts
+++ apps/dnd-5e-api/src/index.ts
@@ -8,4 +8,10 @@ server.get("/", async (request, reply) => {
   };
 });
 
+server.get("/greeting", async (request, reply) => {
+  return {
+    message: "Hello There, General Kenobi",
+  };
+});
+
 server.listen(3000).then(() => console.log("Server Started On PORT 3000"));
