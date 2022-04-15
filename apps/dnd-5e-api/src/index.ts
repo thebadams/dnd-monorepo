18cca6fabf60444bd6746f12cf25ab98d31f25f2
Brian Adams <bpeteradams@gmail.com>
2022-04-14 20:38:40 -0400

    Scripts(dev): wrote basic dev scripts

--- apps/dnd-5e-api/src/index.ts
+++ apps/dnd-5e-api/src/index.ts
@@ -1,3 +1,11 @@
 import fastify from "fastify";
 
 const server = fastify();
+
+server.get("/", async (request, reply) => {
+  return {
+    message: "Hello World",
+  };
+});
+
+server.listen(3000).then(() => console.log("Server Started On PORT 3000"));
