3db7f69977c93b1195bd0d8bc724c8726c183c4a
Brian Adams <bpeteradams@gmail.com>
2022-04-16 22:22:49 -0400

    API(Tests): got a server.test.ts to register

---
+++ apps/dnd-5e-api/test/server.test.ts
@@ -0,0 +1,8 @@
+import { registerServer } from "../src/config/server";
+import { expect } from "chai";
+
+describe("Initial Test", function () {
+  it("Should Pass", function () {
+    expect(true).to.equal(true);
+  });
+});
