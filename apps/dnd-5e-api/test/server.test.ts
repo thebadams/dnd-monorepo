89f631f1b8afe2d99da73676cbcd10210b148e13
Brian Adams <bpeteradams@gmail.com>
2022-04-16 22:49:09 -0400

    API(Tests): Added a test suite for the server config

--- apps/dnd-5e-api/test/server.test.ts
+++
@@ -1,8 +0,0 @@
-import { registerServer } from "../src/config/server";
-import { expect } from "chai";
-
-describe("Initial Test", function () {
-  it("Should Pass", function () {
-    expect(true).to.equal(true);
-  });
-});
