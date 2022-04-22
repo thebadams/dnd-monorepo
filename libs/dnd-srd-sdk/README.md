603f77bab5cb74cb1d3c9158ffc52e3c3e42ef7d
thebadams <bpeteradams@gmail.com>
2022-04-22 13:23:13 -0400

    Migrated srd sdk into monorepo

---
+++ libs/dnd-srd-sdk/README.md
@@ -0,0 +1,11 @@
+# dnd-srd-sdk
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build dnd-srd-sdk` to build the library.
+
+## Running unit tests
+
+Run `nx test dnd-srd-sdk` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ libs/dnd-srd-sdk/jest.config.ts
@@ -0,0 +1,14 @@
+module.exports = {
+  displayName: 'dnd-srd-sdk',
+  preset: '../../jest.preset.ts',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/libs/dnd-srd-sdk',
+};
